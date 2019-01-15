c95aa77c91fadef2ba6c3c154930ddc317c5fb90
Ready Steady <readysteadyitalia@users.noreply.github.com>
2019-01-15 11:25:44 +0100

    elena

---
+++ libretti/versioneDigitale - libretti-separati/contenuto-vaschette/libretti-sinistra/js/magazine.js
@@ -0,0 +1,186 @@
+/*
+ * Magazine sample
+*/
+
+function addPage(page, book) {
+
+	var id, pages = book.turn('pages');
+
+	// Create a new element for this page
+	var element = $('<div />', {});
+
+	// Add the page to the flipbook
+	if (book.turn('addPage', element, page)) {
+
+		// Add the initial HTML
+		// It will contain a loader indicator and a gradient
+		element.html('<div class="gradient"></div><div class="loader"></div>');
+
+		// Load the page
+		loadPage(page, element);
+	}
+
+}
+
+function loadPage(page, pageElement) {
+
+	// Create an image element
+
+	var img = $('<img />');
+
+	img.mousedown(function(e) {
+		e.preventDefault();
+	});
+
+	img.load(function() {
+
+		// Set the size
+		$(this).css({width: '100%', height: '100%'});
+
+		// Add the image to the page after loaded
+
+		$(this).appendTo(pageElement);
+
+		// Remove the loader indicator
+
+		pageElement.find('.loader').remove();
+	});
+
+	// Load the page
+
+	img.attr('src', 'pages/' +  page + '.jpg');
+
+
+
+}
+
+
+
+
+
+
+
+
+
+// http://code.google.com/p/chromium/issues/detail?id=128488
+
+function isChrome() {
+
+	return navigator.userAgent.indexOf('Chrome')!=-1;
+
+}
+
+function disableControls(page) {
+		if (page==1)
+			$('.magazine .previous-button').hide();
+		else
+			$('.magazine .previous-button').show();
+
+		if (page==$('.magazine').turn('pages'))
+			$('.magazine .next-button').hide();
+		else
+			$('.next-button').show();
+}
+
+function disableControls1(page) {
+		if (page==1)
+			$('.magazine1 .previous-button').hide();
+		else
+			$('.magazine1 .previous-button').show();
+
+		if (page==$('.magazine1').turn('pages'))
+			$('.magazine1 .next-button').hide();
+		else
+			$('.next-button').show();
+}
+
+
+
+
+// Number of views in a flipbook
+
+function numberOfViews(book) {
+	return book.turn('pages') / 2 + 1;
+}
+
+// Current view in a flipbook
+
+function getViewNumber(book, page) {
+	return parseInt((page || book.turn('page'))/2 + 1, 10);
+}
+
+
+
+function setPreview(view) {
+
+	var previewWidth = 112,
+		previewHeight = 73,
+		previewSrc = 'pages/preview.jpg',
+		preview = $(_thumbPreview.children(':first')),
+		numPages = (view==1 || view==$('#slider').slider('option', 'max')) ? 1 : 2,
+		width = (numPages==1) ? previewWidth/2 : previewWidth;
+
+
+
+	preview.css({
+		width: width,
+		height: previewHeight
+	});
+
+	if (preview.css('background-image')==='' ||
+		preview.css('background-image')=='none') {
+
+		preview.css({backgroundImage: 'url(' + previewSrc + ')'});
+
+		setTimeout(function(){
+			_thumbPreview.removeClass('no-transition');
+		}, 0);
+
+	}
+
+	preview.css({backgroundPosition:
+		'0px -'+((view-1)*previewHeight)+'px'
+	});
+}
+
+
+
+// decode URL Parameters
+
+function decodeParams(data) {
+
+	var parts = data.split('&'), d, obj = {};
+
+	for (var i =0; i<parts.length; i++) {
+		d = parts[i].split('=');
+		obj[decodeURIComponent(d[0])] = decodeURIComponent(d[1]);
+	}
+
+	return obj;
+}
+
+// Calculate the width and height of a square within another square
+
+function calculateBound(d) {
+
+	var bound = {width: d.width, height: d.height};
+
+	if (bound.width>d.boundWidth || bound.height>d.boundHeight) {
+
+		var rel = bound.width/bound.height;
+
+		if (d.boundWidth/rel>d.boundHeight && d.boundHeight*rel<=d.boundWidth) {
+
+			bound.width = Math.round(d.boundHeight*rel);
+			bound.height = d.boundHeight;
+
+		} else {
+
+			bound.width = d.boundWidth;
+			bound.height = Math.round(d.boundWidth/rel);
+
+		}
+	}
+
+	return bound;
+}
