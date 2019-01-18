0a6d76d218144a3333fd6ba731459062d3df7139
elegib <elena.gibby97@gmail.com>
2019-01-18 15:27:11 +0100

    biblio-completa

--- mobile/dati-testi.js
+++ mobile/dati-testi.js
@@ -1,16 +1,16 @@
 var text = '{"testi":[' +
-'{"verità":"John","testi":"Doe" },' +
-'{"verità":"John","testi":"Doe" },' +
-'{"verità":"John","testi":"Doe" },' +
-'{"verità":"John","testi":"Doe" },' +
-'{"verità":"John","testi":"Doe" },' +
-'{"verità":"John","testi":"Doe" },' +
-'{"verità":"John","testi":"Doe" },' +
-'{"verità":"John","testi":"Doe" },' +
-'{"verità":"John","testi":"Doe" },' +
-'{"verità":"John","testi":"Doe" },' +
-'{"verità":"LIBERI DI SCEGLIERE LA MORTE","testi":"Doe" },' +
-'{"verità":"L&#39 UTERO &Egrave MIO E LO GESTISCO IO","testi":"Doe" },' +
-'{"verità":"NON C&#39 &Egrave BISOGNO DI CONFINI","testi":"Doe" },' +
-'{"verità":"L&#39 ITALIA AGLI ITALIANI","testi":"Alfredo Rocco, Il programma politico dell&#39 associazione nazionalista italiana, 1919 <br><br> Direzione generale della Comunicazione (Commissione europea), 60 buone ragioni per cui abbiamo bisogno dell’Unione europea, Bruxelles, 2017" },' +
-'{"verità":"L&#39 AMORE NON HA SESSO","testi":"Jones" }]}';
+'{"verita":"John","testi":"Doe" },' +
+'{"verita":"John","testi":"Doe" },' +
+'{"verita":"John","testi":"Doe" },' +
+'{"verita":"John","testi":"Doe" },' +
+'{"verita":"John","testi":"Doe" },' +
+'{"verita":"John","testi":"Doe" },' +
+'{"verita":"John","testi":"Doe" },' +
+'{"verita":"John","testi":"Doe" },' +
+'{"verita":"John","testi":"Doe" },' +
+'{"verita":"John","testi":"Doe" },' +
+'{"verita":"LIBERI DI SCEGLIERE LA MORTE","testi":"Ippocrate, Giuramento di Ippocrate, IV secolo a.C.<br><br>Franjo Cardinale Seper, Dichiarazione sull’eutanasia, Roma, sede della Sacra Congregazione per la Dottrina della Fede, 1980<br><br>Consiglio Pontificio,Questioni etiche relative ai malati gravi e morenti,Roma, 1981<br><br>Il quinto comandamento, in Catechismo della Chiesa, sezione seconda, capitolo secondo, articolo 5, 1997<br><br>Mauro Ronco, L’indisponibilità della vita: assolutizzazione del principio autonomistico e svuotamento della tutela penale della vita, in Cristianità n. 341-342 maggio-agosto, 2007<br><br>Beppino Englaro, La vita estrema, Beppino Englaro in conversazione con Piero Colaprico, da MicroMega, 2009" },' +
+'{"verita":"L&#39 UTERO &Egrave MIO E LO GESTISCO IO","testi":"The Boston Women’s health book collective, Noi e il nostro corpo scritto dalle donne per le donne, Feltrinelli, Milano 1977<br><br>Vincenzo Gubitosi, Autodeterminazione femminista e delirio di onnipotenza, in Notizie Provita 2018" },' +
+'{"verita":"NON C&#39 &Egrave BISOGNO DI CONFINI","testi":"Paul Eluard, Scrivo il tuo nome, Libertà, in Poesia e verità, in Poesie, trad. it. di F. Fortini, Mondadori, Milano 1969<br><br>Immanuel Kant, Per la pace perpetua (parte seconda, secondo articolo definitivo), 1975<br><br>Marco Gervasoni, Altro che guerrafondaio. Il nazionalismo è la virtù di chi resiste agli Imperi, in Il Giornale.it, 2018" },' +
+'{"verita":"L&#39 ITALIA AGLI ITALIANI","testi":"Alfredo Rocco, Il programma politico<br>dell&#39 associazione nazionalista italiana, 1919 <br><br> Direzione generale della Comunicazione (Commissione europea), 60 buone ragioni per cui abbiamo bisogno dell’Unione europea, Bruxelles 2017" },' +
+'{"verita":"L&#39 AMORE NON HA SESSO","testi":"Simona Argentieri, A qualcuno piace uguale, Einaudi, Torino 2010<br><br>Franca Giansoldati, Papa Francesco: «Sesso dono di Dio da vivere senza tabù, ma solo tra uomo e donna», in Il Messaggero.it, 2018" }]}';
