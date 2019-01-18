30f83fca380d9e9d30eb13f6a320f8f9f3a71e66
elegib <elena.gibby97@gmail.com>
2019-01-18 11:13:15 +0100

    biblio

---
+++ mobile/dati-testi.js
@@ -0,0 +1,16 @@
+var text = '{"testi":[' +
+'{"verità":"John","testi":"Doe" },' +
+'{"verità":"John","testi":"Doe" },' +
+'{"verità":"John","testi":"Doe" },' +
+'{"verità":"John","testi":"Doe" },' +
+'{"verità":"John","testi":"Doe" },' +
+'{"verità":"John","testi":"Doe" },' +
+'{"verità":"John","testi":"Doe" },' +
+'{"verità":"John","testi":"Doe" },' +
+'{"verità":"John","testi":"Doe" },' +
+'{"verità":"John","testi":"Doe" },' +
+'{"verità":"LIBERI DI SCEGLIERE LA MORTE","testi":"Doe" },' +
+'{"verità":"L&#39 UTERO &Egrave MIO E LO GESTISCO IO","testi":"Doe" },' +
+'{"verità":"NON C&#39 &Egrave BISOGNO DI CONFINI","testi":"Doe" },' +
+'{"verità":"L&#39 ITALIA AGLI ITALIANI","testi":"Smith" },' +
+'{"verità":"L&#39 AMORE NON HA SESSO","testi":"Jones" }]}';
