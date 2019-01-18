ead0304f55c05534b60137b27efab6f4509bd14f
elegib <elena.gibby97@gmail.com>
2019-01-18 11:51:06 +0100

    biblio1

--- mobile/dati-testi.js
+++ mobile/dati-testi.js
@@ -12,5 +12,5 @@ var text = '{"testi":[' +
 '{"verità":"LIBERI DI SCEGLIERE LA MORTE","testi":"Doe" },' +
 '{"verità":"L&#39 UTERO &Egrave MIO E LO GESTISCO IO","testi":"Doe" },' +
 '{"verità":"NON C&#39 &Egrave BISOGNO DI CONFINI","testi":"Doe" },' +
-'{"verità":"L&#39 ITALIA AGLI ITALIANI","testi":"Smith" },' +
+'{"verità":"L&#39 ITALIA AGLI ITALIANI","testi":"Alfredo Rocco, Il programma politico dell&#39 associazione nazionalista italiana, 1919 <br><br> Direzione generale della Comunicazione (Commissione europea), 60 buone ragioni per cui abbiamo bisogno dell’Unione europea, Bruxelles, 2017" },' +
 '{"verità":"L&#39 AMORE NON HA SESSO","testi":"Jones" }]}';
